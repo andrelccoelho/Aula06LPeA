60a185668acb18ef90e311a938354a18d03eccc1
andrelccoelho <andrenomics@gmail.com>
2026-07-14 14:23:54 -0300

    Adiciona exercicios da aula 5

--- Aula_5_Funções/exerc_aulaT5_4.68.py
+++ Aula_5_Funções/exerc_aulaT5_4.68.py
@@ -1,7 +1,23 @@
-def fat (num):
+def valida_int(pergunta, min, max):
+    x = int(input(pergunta))
+    while((x < min) or (x > max)):
+        x = int(input(pergunta))
+    return x
+
+def fatorial (num):
+    """
+    Função que calcula a fatorial de um número inteiro.
+    :param num:
+    :return:
+    """
     fat = 1
-    if num == 0
+    if num == 0:
         return fat
     # esta parte só executa caso num > 0
-    for 1 in range()
-    
+    for i in range(1, num + 1, 1):
+        fat *= i
+    return fat
+
+x = valida_int('Digite um valor para calcular a fatorial: ', 0, 99999)
+print(f'{x}! = {fatorial(x)}')
+help(fatorial)
